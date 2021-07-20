9b79c0a16f92df6c12ebd796a4a0de48121aa5c5
A-Mitsunaga <a56st70410@icloud.com>
2021-07-20 09:41:04 +0000

    [Add]いいね、コメント

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
-  resources :post_images, only:[:new, :create, :index, :show, :destroy]
+
+  #ネスト
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+    resource :favorites, only:[:create, :destroy]
+    resources :post_comments, only:[:create, :destroy]
+  end
+
 end
