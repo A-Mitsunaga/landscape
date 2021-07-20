58020d06cd9b695fead40ff3217333cb3adf5071
A-Mitsunaga <a56st70410@icloud.com>
2021-07-20 08:58:35 +0000

    [Add]画像投稿機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後（投稿画面一覧）
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+
   protected
 
   def configure_permitted_parameters
---
+++ app/models/post_image.rb
@@ -0,0 +1,6 @@
+class PostImage < ApplicationRecord
+   belongs_to :user
+   attachment :image
+   has_many :post_comments, dependent: :destroy
+
+end
