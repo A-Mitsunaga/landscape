83bd654be813e4cf1afee21034860fc05e9a3cdf
A-Mitsunaga <a56st70410@icloud.com>
2021-07-20 09:40:47 +0000

    [Add]いいね、コメント

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_085238) do
+ActiveRecord::Schema.define(version: 2021_07_20_092538) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
