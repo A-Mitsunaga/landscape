4bc8324a42acd17c3ab100ff7d1f4096f27f7d5b
A-Mitsunaga <a56st70410@icloud.com>
2021-07-20 08:59:36 +0000

    [Add]画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ba37f74eb736dba963b2a4c5b1129ddc3a725db9a069ea83823640e822aad9d1a13d611b4bcf2c3e6b9272f3c34b6d6366decadfdf441e6dd5936b6f8308c87d'
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
+  resources :post_images, only:[:new, :create, :index, :show, :destroy]
 end
