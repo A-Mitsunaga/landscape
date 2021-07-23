73caca2548c159544bd1f4d0dcad69701563da9e
A-Mitsunaga <a56st70410@icloud.com>
2021-07-23 19:57:12 +0000

    [Add]基本的な部分

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,26 @@ class User < ApplicationRecord
    has_many :post_images, dependent: :destroy
    has_many :post_comments, dependent: :destroy
    has_many :favorites, dependent: :destroy
+   attachment :profile_image
+
+  #フォロー
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 被フォロー関係を通じて参照→followed_idをフォローしている人
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 【class_name: "Relationship"】は省略可能
+  has_many :followings, through: :relationships, source: :followed
+  # 与フォロー関係を通じて参照→follower_idをフォローしている人
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    followings.include?(user)
+  end
 
 end
