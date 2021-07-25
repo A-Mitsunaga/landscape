29437510b479990e625ce63a4a6d8718a25a2cae
A-Mitsunaga <a56st70410@icloud.com>
2021-07-25 06:11:29 +0000

    [Add]お問い合わせ機能gemfile入れる前

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_045726) do
+ActiveRecord::Schema.define(version: 2021_07_25_060459) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.text "message", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
