12e703531d9b539ad120a8ce379fd34b2ea09887
A-Mitsunaga <a56st70410@icloud.com>
2021-08-02 15:48:42 +0000

    [DELETE]フォロー機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+before_action :authenticate_user!
+before_action :set_user, only: %I[show edit update destroy followings followers]
+
   def index
     @users = User.all
   end
@@ -20,9 +23,23 @@ class UsersController < ApplicationController
     redirect_to user_path(@user.id)
   end
 
+  def followings
+    @followings = @user.following_users
+  end
+
+  def followers
+    @followers = @user.follower_users
+  end
+
+
    private
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,31 @@ class User < ApplicationRecord
    has_many :favorites, dependent: :destroy
    attachment :profile_image
 
+   has_many :followers,
+            class_name: 'Relationship',
+            foreign_key: 'follower_id',
+            dependent: :destroy,
+            inverse_of: :follower
+
+  has_many :following,
+            class_name: 'Relationship',
+            foreign_key: 'following_id',
+            dependent: :destroy,
+            inverse_of: :following
+
+  has_many :following_users, through: :followers, source: :following
+  has_many :follower_users, through: :following, source: :follower
+
+  #フォローする
+    def follow(other_user_id)
+      followers.create(following_id: other_user_id)
+    end
+
+    #フォローしているかの確認
+    def following?(other_user)
+      following_users.include?(other_user)
+    end
+
+  #フォロー解除
+
 end
